8e451cdd70bf889e59d97b5be79d5c8dc32c46a8
manirampa20 <pacifique.manirampa@amalitechtraining.org>
2024-07-22 21:44:23 +0200

    commit with jacoco

--- LibraryManagementWithTest/LibraryManagementWithTest/src/test/java/com/LibraryManagement/LibraryManagementWithTest/TransactionDAOTest.java
+++ LibraryManagementWithTest/LibraryManagementWithTest/src/test/java/com/LibraryManagement/LibraryManagementWithTest/TransactionDAOTest.java
@@ -1,10 +1,12 @@
 package com.LibraryManagement.LibraryManagementWithTest;
 
 import com.LibraryManagement.LibraryManagementWithTest.dao.TransactionDAO;
+import com.LibraryManagement.LibraryManagementWithTest.database.LibraryManager;
 import com.LibraryManagement.LibraryManagementWithTest.models.Transaction;
 import org.junit.jupiter.api.*;
 
 import java.sql.Date;
+import java.sql.SQLException;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -16,13 +18,38 @@ public class TransactionDAOTest {
     @BeforeAll
     public void setUp() {
         transactionDAO = new TransactionDAO();
+        try (var conn = LibraryManager.getConnection()) {
+            String createBookTableSQL = "CREATE TABLE IF NOT EXISTS book ("
+                    + "bookId INT AUTO_INCREMENT, "
+                    + "title VARCHAR(255), "
+                    + "author VARCHAR(255), "
+                    + "isbn VARCHAR(13), "
+                    + "genre VARCHAR(255), "
+                    + "copiesAvailable INT, "
+                    + "isAvailable BOOLEAN, "
+                    + "PRIMARY KEY (bookId))";
+            conn.createStatement().execute(createBookTableSQL);
+
+            String createTransactionTableSQL = "CREATE TABLE IF NOT EXISTS transaction ("
+                    + "transactionId INT AUTO_INCREMENT, "
+                    + "bookId INT, "
+                    + "fullName VARCHAR(255), "
+                    + "borrowDate  DATE,"
+                    + "dueDate DATE, "
+                    + "returnDate DATE, "
+                    + "status VARCHAR(100),"
+                    + "PRIMARY KEY (transactionId))";
+            conn.createStatement().execute(createTransactionTableSQL);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     @Test
     public void testInsertTransaction() {
 
 
-        Transaction transaction = new Transaction(33, 1, "John Doe", "2024-07-01", "2024-08-01", "2024-07-15", "BORROWED");
+        Transaction transaction = new Transaction(65, 1, "John Doe", "2024-07-01", "2024-08-01", "2024-07-15", "BORROWED");
         transactionDAO.insert(transaction);
         Transaction retrievedTransaction = transactionDAO.getTransactionById(transaction.getTransactionId());
         assertNotNull(retrievedTransaction);
@@ -38,7 +65,7 @@ public class TransactionDAOTest {
 
     @Test
     public void testUpdateTransaction() {
-        Transaction transaction = new Transaction(31, 1, "Jane Doe", "2024-06-01", "2024-07-01", "2024-06-20", "BORROWED");
+        Transaction transaction = new Transaction(60, 1, "Jane Doe", "2024-06-01", "2024-07-01", "2024-06-20", "BORROWED");
         transactionDAO.insert(transaction);
         transaction.setStatus("RETURNED");
         transactionDAO.updateTransaction(transaction);
